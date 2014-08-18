5a3d89262194dc6016fd36b831e5b8c5d086ed01
abiusx <git@abiusx.com>
2014-08-18 10:50:31 -0400

    first working version of puretext

---
+++ puretext/example1.php
@@ -0,0 +1,10 @@
+<?php
+require_once __DIR__."/pure_text_render.php";
+$obj=new PureTextRender();
+$obj->render_text("Vote on PHP Programming Innovation award
+
+This award is an initiative meant to provide recognition to the authors that contribute with the most innovating classes.
+
+In the beginning of every month the nominees are announced on the vote page.
+
+In the following month, the authors of the most voted classes may win prizes provided by the following sponsors");
